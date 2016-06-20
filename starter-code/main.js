627ee0bd2511b1419774e540dfd2d5a94d61b679
Aubrey Northam <aubreynortham@aubrey-northams-macbook-pro.local>
2016-06-20 14:33:59 -0400

    modified the js file by adding game logic in unit 9 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,10 @@
-console.log("JS file is connected to HTML! Woo!")
-
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+
+if (cardOne === cardFour) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
