7af63ad07f37a4c7857b232783af04f88b112530
Aubrey Northam <aubreynortham@aubrey-northams-macbook-pro.local>
2016-06-21 14:18:44 -0400

    modified html file and js file for unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+//var cardOne = "queen";
+//var cardTwo = "king";
+//var cardThree = "king";
+//var cardFour = "queen";
 
-if (cardOne === cardFour) {
+/*if (cardOne === cardFour) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
+}
+*/
+
+var board = document.getElementById('game-board');
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    board.appendChild(cardElement);
+  }
 }
